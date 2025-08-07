8a7c71a06ee12fc7a7e9e662d738a374601c3983
shubhi awasthi13 <awasthishubhi147@gmail.com>
2025-08-07 19:51:52 +0530

    :integrated hosted backend API

--- Client/src/services/api.js
+++ Client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://whatsapp-web-clone-rt2t.onrender.com/api',
 });
 
 export const fetchMessages = () => API.get('/messages');
--- Client/src/services/socket.js
+++ Client/src/services/socket.js
@@ -1,4 +1,4 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:5000');
+const socket = io('https://whatsapp-web-clone-rt2t.onrender.com');
 export default socket;
